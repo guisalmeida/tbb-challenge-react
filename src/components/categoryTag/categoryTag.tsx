1a04aa01f8de843d69f7fb812fac312d34f2c070
guisalmeida <guisalmeida.dev@gmail.com>
2023-12-09 16:58:40 -0300

    feat: create categoryTag component

---
+++ src/components/categoryTag/categoryTag.tsx
@@ -0,0 +1,22 @@
+import closeIcon from "../../assets/close-icon.svg";
+import './categoryTag.scss'
+
+type CategoryTagType = {
+  category: string;
+};
+
+const categoryTag = ({ category }: CategoryTagType) => {
+  return (
+    <button
+      type="button"
+      className="category-tag"
+      title="Remover categoria"
+      aria-label="Remover categoria"
+    >
+      {category}
+      <img src={closeIcon} alt="Close icon" />
+    </button>
+  );
+};
+
+export default categoryTag;
