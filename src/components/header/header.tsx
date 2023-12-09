39dd7f20a7e7371135bed483f7719cc9416c4752
guisalmeida <guisalmeida.dev@gmail.com>
2023-12-09 18:30:20 -0300

    feat: create header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,28 @@
+import CategoryTag from '../categoryTag/categoryTag'
+import SearchInput from '../searchInput/searchInput'
+import searchImage from "/images/search.svg";
+import './header.scss'
+
+const header = () => {
+  return (
+    <header className="search-header__container">
+        <figure className="search-header__image">
+          <img src={searchImage} alt="A man searching with a glass" />
+        </figure>
+        <h1>
+          O que você <span>está procurando?</span>
+        </h1>
+
+        <section className="search-input--desktop">
+          <SearchInput />
+        </section>
+
+        <div className="search-header__bottom">
+          <CategoryTag category="teste" />
+          <CategoryTag category="teste" />
+        </div>
+      </header>
+  )
+}
+
+export default header
--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,51 +1,30 @@
 import ProductCard from "../productCard/productCard";
-import searchIcon from "/images/search-icon.svg";
+import Filters from "../searchFilters/searchFilters";
+import searchImage from "/images/search.svg";
+import filterIcon from "../../assets/filter-icon.svg";
 
+import Header from "../header/header";
+import SearchInput from "../searchInput/searchInput";
 import "./search.scss";
 
 const search = () => {
   return (
     <section className="search__container">
       <figure className="search__image">
-        <img src={searchIcon} alt="test" />
+        <img src={searchImage} alt="A man searching with a glass" />
       </figure>
-      <header className="search-header__container">
-        <h1>
-          O que você <span>está procurando?</span>
-        </h1>
 
-        <input type="text" placeholder="Busque aqui" />
+      <Header />
 
-        <div className="search-header__bottom">
-          <button className="search-filters__button">Category</button>
-          <button className="search-filters__button">Category</button>
-          <button className="search-filters__button">Category</button>
-          <button className="search-filters__button">Category</button>
-          <button className="search-filters__button">Category</button>
-          <button className="search-filters__button">Category</button>
-          <button className="search-filters__button">Category</button>
-          <button className="search-filters__button">Category</button>
-        </div>
-      </header>
+      <section className="search-input--mobile">
+        <button className="search-input__button">
+          <img src={filterIcon} alt="Filter icon" />
+        </button>
+        <SearchInput />
+      </section>
 
-      <aside className="search-filters__container">
-        <h2>
-          Filtros
-          <span className="search-filters__results">123 resultados</span>
-        </h2>
-        <nav>
-          <ul>
-            <li>
-              <input type="checkbox" id="category" name="category" />
-              <label htmlFor="category">category1 (22)</label>
-            </li>
-            <li>
-              <input type="checkbox" id="category2" name="category2" />
-              <label htmlFor="category2">category2 (33)</label>
-            </li>
-          </ul>
-        </nav>
-      </aside>
+      <Filters />
+
       <main>
         <div className="search-products__container">
           <ProductCard />
